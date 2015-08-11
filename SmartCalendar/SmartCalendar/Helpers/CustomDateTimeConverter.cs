900a1f331dab040f74ebeea696e51ee566d2aed1
GenPike <olexis552@gmail.com>
2015-08-11 14:20:51 +0300

    change formatting of datetime types

---
+++ SmartCalendar/SmartCalendar/Helpers/CustomDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SmartCalendar.Helpers
+{
+    public class CustomDateTimeConvertor : DateTimeConverterBase
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return DateTime.Parse(reader.Value.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((DateTime)value).ToString("MM-dd-yyyy HH:mm"));
+        }
+    }
+}
