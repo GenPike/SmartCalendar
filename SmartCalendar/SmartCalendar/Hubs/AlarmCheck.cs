8d2444c1c04a9d3d527fa1fa0430e2844e2cd5ad
GenPike <olexis552@gmail.com>
2015-08-14 13:57:49 +0300

    add hub and logic to notify user

---
+++ SmartCalendar/SmartCalendar/Hubs/AlarmCheck.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.SignalR;
+using SmartCalendar.Models;
+using SmartCalendar.Models.Abstracts;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace SmartCalendar.Hubs
+{
+    public class AlarmCheck : ApiController, IRegisteredObject
+    {
+        private readonly IHubContext _uptimeHub;
+        private Timer _timer;
+        public DateTime StartDate = DateTime.Now;
+        int x = 0;
+
+        private IRepository repository;
+
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public AlarmCheck(IRepository repos)
+        {
+            repository = repos;
+        }
+
+        public AlarmCheck()
+          
+        {
+            _uptimeHub = GlobalHost.ConnectionManager.GetHubContext<AlarmHub>();
+
+            StartTimer();
+        }
+
+
+        private void StartTimer()
+        {
+            var delayStartby = 1000;
+            var repeatEvery = 6000;
+
+            _timer = new Timer(BroadcastUptimeToClients, null, delayStartby, repeatEvery);
+        }
+
+        private void BroadcastUptimeToClients(object state)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var res = db.Events.Where(e => e.UserId == userId && (e.DateStart >= StartDate))
+                .ToList()
+                .Where(e => e.DateStart <= DateTime.Now.AddDays(2))
+                .OrderBy(e => e.DateStart);
+            
+            foreach (var e in res)
+            {
+                if (e != null )
+                {
+                    string str = e.DateStart.ToString("g");
+
+                    //if (DateTime.Now.AddMinutes(5).ToString("g").Equals(str))
+                        _uptimeHub.Clients.All.addNewMessageToPage(e.Title, e.DateStart.ToString("g"));
+                    break;
+                }
+            }
+            
+        }
+
+        public void Stop(bool immediate)
+        {
+            _timer.Dispose();
+
+            HostingEnvironment.UnregisterObject(this);
+        }
+    }
+}
---
+++ SmartCalendar/SmartCalendar/Hubs/AlarmHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SmartCalendar.Hubs
+{
+    public class AlarmHub : Hub
+    {
+
+        //    static List<User> Users = new List<User>();
+        // Отправка сообщений
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        // Подключение нового пользователя
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+            {
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+    }
+}
